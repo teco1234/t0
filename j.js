a1d37f88d2bb7a08eb8c464b0c047e731a6e4d6f
teco1234 <tecosoueu@outlook.com>
2025-07-24 19:03:22 -0300

    atu

---
+++ j.js
@@ -0,0 +1,18 @@
+function mt(a,b){return Math.floor(Math.random()*a)+b}
+function al(a){alert(a)}
+function cl(a){console.log(a)}
+function cr(a){return document.createElement(a)}
+function st(a){a.sort(()=>Math.random()-.5)}
+
+function fr(a){
+    for(let i=0;i<a.length;i++)a[i]=i
+}
+function mfr(a){
+    for(let i=0;i<a.length;i++)cl(a[i])
+}
+function ie(a,b){a.innerHTML=b}
+
+function nn(a){a.style.display="none"}
+function bl(a){a.style.display="block"}
+
+function bk(a,c){a.style.backgroundColor=c}
