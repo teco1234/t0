120f2d3d4297a4673c28231e5af0f01bc2db7fd2
teco1234 <tecosoueu@outlook.com>
2025-07-25 18:00:48 -0300

    jogo 2

--- j.js
+++ j.js
@@ -1,19 +1,39 @@
-function mt(a,b){return Math.floor(Math.random()*a)+b}
-function al(a){alert(a)}
-function cl(a){console.log(a)}
-function cr(a){return document.createElement(a)}
-function st(a){a.sort(()=>Math.random()-.5)}
 
-function fr(a){
+
+
+o=['black','red','blue','white']
+u=['-','|','Calcule!','Resultado','Acertou!','Errou!']
+
+function mat(a,b){return Math.floor(Math.random()*a)+b}
+function al(a){alert(a)}
+function cn(a){console.log(a)}
+function mostra(a){
+    for(let i=0;i<a.length;i++)cn(a[i])
+}
+function criar(a){return document.createElement(a)}
+
+function insere(a){
     for(let i=0;i<a.length;i++)a[i]=i
 }
-function mfr(a){
-    for(let i=0;i<a.length;i++)cl(a[i])
+
+function tx(a,b){a.innerHTML=b}
+
+function tln(a){a.style.display="none"}
+function tlb(a){a.style.display="block"}
+function tli(a){a.style.display="inline"}
+
+function fd(a,c){a.style.backgroundColor=c}
+function ft(a,c){a.style.color=c}
+
+function soma(a,b){return a+b}
+function sub(a,b){return a-b}
+function mult(a,b){return a*b}
+function div(a,b){return a/b}
+
+function ise(a){
+    for(let i=0;i<a.length;i++)a[i]=i
 }
-function ie(a,b){a.innerHTML=b}
-
-function nn(a){a.style.display="none"}
-function bc(a){a.style.display="block"}
-function bl(a){a.style.display="inline"}
-
-function bk(a,c){a.style.backgroundColor=c}
+function sorte(a){a.sort(()=>Math.random()-.5)}
+function mostra(a){
+    for(let i=0;i<a.length;i++)cn(a[i])
+}
